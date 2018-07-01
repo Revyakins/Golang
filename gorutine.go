fe963494a6b9bc21c92d23d5ae3509c220ca1934
revyakins <scoundrel1992@gmail.com>
2018-07-02 00:24:23 +0300

    refactor

--- gorutine.go
+++ gorutine.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	//"time"
-	//"math/rand"
+	"time"
+	"math/rand"
 )
 
 
 func f(n int) {
 	for i :=0; i<10; i++ {
 		fmt.Println(n, ":", i)
-		//amt := time.Duration(rand.Intn(250))
-		//time.Sleep(time.Millisecond * amt)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
 	}
 }
 
